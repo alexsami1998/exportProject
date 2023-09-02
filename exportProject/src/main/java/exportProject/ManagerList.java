a7c8730593638f3a723bcd117c541f2292135da8
alexsami1998 <alexandre-enter86@live.com>
2023-09-02 00:41:19 -0300

    metodo de busca de dados

---
+++ exportProject/src/main/java/exportProject/ManagerList.java
@@ -0,0 +1,38 @@
+package exportProject;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ManagerList {
+
+	public static void ManagerList() {
+        try (FileInputStream fis = new FileInputStream("dados.xlsx");
+             Workbook workbook = new XSSFWorkbook(fis)) {
+
+            Sheet sheet = workbook.getSheet("Dados");
+
+            if (sheet != null) {
+                for (Row row : sheet) {
+                    for (org.apache.poi.ss.usermodel.Cell cell : row) {
+                        System.out.print(cell.toString() + "\t");
+                    }
+                    System.out.println();
+                }
+            } else {
+                System.out.println("A planilha 'Dados' não existe no arquivo.");
+            }
+
+        } catch (IOException e) {
+            System.out.println("Erro ao abrir o arquivo: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        ManagerList();
+    }
+}
