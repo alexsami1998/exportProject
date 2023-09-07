6564c41ffb3b97fc925de332eab761afaace0428
alexsami1998 <alexandre-enter86@live.com>
2023-09-07 17:04:45 -0300

    Ajuste de migracao integrada.

--- exportProject/src/main/java/exportProject/SearchOpByData.java
+++ exportProject/src/main/java/exportProject/SearchOpByData.java
@@ -2,6 +2,7 @@ package exportProject;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.Scanner;
@@ -15,57 +16,87 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class SearchOpByData {
 
-	 public static void searchAndDisplayDataByOP(String op) {
-		 String ref = null;
-		 String opN = op;
-		 String qntd = null;
-		 String sts = null;
-	        try {
-	            FileInputStream fis = new FileInputStream(new File("dados.xlsx"));
-				Workbook workbook = new XSSFWorkbook(fis);
-	            Sheet sheet = workbook.getSheet("Entrada");
+    public static void searchAndDisplayDataByOP(String op) {
+        String ref = null;
+        String opN = op;
+        String qntd = null;
+        String sts = null;
+        try {
+            FileInputStream fis = new FileInputStream(new File("dados.xlsx"));
+            Workbook workbook = new XSSFWorkbook(fis);
+            Sheet sheet = workbook.getSheet("Entrada");
 
-	            Iterator<Row> iterator = sheet.iterator();
+            Iterator<Row> iterator = sheet.iterator();
 
-	            while (iterator.hasNext()) {
-	                Row currentRow = iterator.next();
-	                Cell opCell = currentRow.getCell(3); // Supondo que a coluna da OP seja a quarta coluna (índice 3)
+            while (iterator.hasNext()) {
+                Row currentRow = iterator.next();
+                Cell opCell = currentRow.getCell(2); // Coluna OP
 
-	                if (opCell != null && opCell.getCellType() == CellType.STRING) {
-	                    String opValue = opCell.getStringCellValue();
+                if (opCell != null && opCell.getCellType() == CellType.STRING) {
+                    String opValue = opCell.getStringCellValue();
 
-	                    if (opValue.equals(op)) {
-	                        // Se a OP encontrada corresponder à OP fornecida
-	                        System.out.println("Data: " + currentRow.getCell(0));
-	                        System.out.println("Referencia: " + currentRow.getCell(1));
-	                        System.out.println("OP: " + currentRow.getCell(2));
-	                        System.out.println("Quantidade: " + opValue);
-	                        System.out.println("FRENT: " + currentRow.getCell(4));
-	                        System.out.println("TRAS: " + currentRow.getCell(5));
-	                        System.out.println("SIL/BOR: " + currentRow.getCell(6));
-	                        System.out.println("Kamb: " + currentRow.getCell(7));
-	                        System.out.println("Status: " + currentRow.getCell(8));
-	                        return; // Encontrou a linha, não é necessário continuar a busca
-	                    }
-	                }
-	            }
+                    if (opValue.equals(op)) {
+                        // Se a OP encontrada corresponder à OP fornecida
+                        ref = currentRow.getCell(1).getStringCellValue();
+                        qntd = currentRow.getCell(3).getStringCellValue();
+                        sts = currentRow.getCell(8).getStringCellValue();
+                        System.out.println("Data: " + currentRow.getCell(0));
+                        System.out.println("Referencia: " + ref);
+                        System.out.println("OP: " + opValue);
+                        System.out.println("Quantidade: " + qntd);
+                        System.out.println("FRENT: " + currentRow.getCell(4));
+                        System.out.println("TRAS: " + currentRow.getCell(5));
+                        System.out.println("SIL/BOR: " + currentRow.getCell(6));
+                        System.out.println("Kamb: " + currentRow.getCell(7));
+                        System.out.println("Status: " + sts);
+                        workbook.close();
+                        fis.close();
+                        transferDataToAlocation(ref, opN, qntd, sts); // Chama a função para transferir dados
+                        return; // Encontrou a linha, não é necessário continuar a busca
+                    }
+                }
+            }
 
-	            // Se não encontrou uma linha com a OP fornecida
-	            System.out.println("Nenhuma entrada encontrada para a OP: " + op);
+            // Se não encontrou uma linha com a OP fornecida
+            System.out.println("Nenhuma entrada encontrada para a OP: " + op);
 
-	            workbook.close();
-	            fis.close();
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        }
-	    }
+            workbook.close();
+            fis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	    public static void main(String[] args) {
-			Scanner scanner = new Scanner(System.in);
-	        System.out.print("Digite a OP desejada: ");
-	        String opDesejada = scanner.nextLine();
+    public static void transferDataToAlocation(String ref, String op, String qntd, String sts) {
+        try {
+            FileInputStream fis = new FileInputStream(new File("dados.xlsx"));
+            Workbook workbook = new XSSFWorkbook(fis);
+            Sheet sheet = workbook.getSheet("Alocacao");
 
-	        searchAndDisplayDataByOP(opDesejada);
-	    }
-	
+            int lastRowNum = sheet.getLastRowNum();
+            Row dataRow = sheet.createRow(lastRowNum + 1);
+            dataRow.createCell(0).setCellValue(ref);
+            dataRow.createCell(1).setCellValue(op);
+            dataRow.createCell(2).setCellValue(qntd);
+            dataRow.createCell(3).setCellValue(sts);
+
+            try (FileOutputStream fileOut = new FileOutputStream("dados.xlsx")) {
+                workbook.write(fileOut);
+                System.out.println("Dados transferidos para a planilha Alocacao.");
+            }
+
+            workbook.close();
+            fis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite a OP desejada: ");
+        String opDesejada = scanner.nextLine();
+
+        searchAndDisplayDataByOP(opDesejada);
+    }
 }
