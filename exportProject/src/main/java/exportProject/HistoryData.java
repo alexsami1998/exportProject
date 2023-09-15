0d9f53c449cdb3b98a839b801c2e2c785e42e304
alexsami1998 <alexandre-enter86@live.com>
2023-09-15 16:50:10 -0300

    Classe para registro de todos os dados de movimentacoes de controle #14

--- exportProject/src/main/java/exportProject/HistoryData.java
+++ exportProject/src/main/java/exportProject/HistoryData.java
@@ -1,15 +1,11 @@
 package exportProject;
 
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Date;
-import java.util.Iterator;
-import java.util.Scanner;
 
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -17,73 +13,141 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class HistoryData {
 
-    public static void migrateOp(String op, String sourceSheetName, String targetSheetName) {
-        try {
-            FileInputStream fis = new FileInputStream(new File("dados.xlsx"));
-            Workbook workbook = new XSSFWorkbook(fis);
-            Sheet sourceSheet = workbook.getSheet(sourceSheetName);
-            Sheet targetSheet = workbook.getSheet(targetSheetName);
-
-            Iterator<Row> iterator = sourceSheet.iterator();
-
-            while (iterator.hasNext()) {
-                Row currentRow = iterator.next();
-                Cell opCell = currentRow.getCell(2); // Coluna OP
-
-                if (opCell != null && opCell.getCellType() == CellType.STRING) {
-                    String opValue = opCell.getStringCellValue();
-
-                    if (opValue.equals(op)) {
-                        // Encontrou a OP na planilha de origem
-                        // Mova a linha para a planilha de destino
-                        Row newRow = targetSheet.createRow(targetSheet.getLastRowNum() + 1);
-                        Iterator<Cell> cellIterator = currentRow.cellIterator();
-                        int cellIndex = 0;
-                        while (cellIterator.hasNext()) {
-                            Cell cell = cellIterator.next();
-                            Cell newCell = newRow.createCell(cellIndex++);
-                            newCell.setCellValue(cell.getStringCellValue());
-                        }
-
-                        // Registre a migração no histórico
-                        Sheet historySheet = workbook.getSheet("Historico");
-                        Row historyRow = historySheet.createRow(historySheet.getLastRowNum() + 1);
-                        historyRow.createCell(0).setCellValue(op);
-                        historyRow.createCell(1).setCellValue(sourceSheetName);
-                        historyRow.createCell(2).setCellValue(targetSheetName);
-                        historyRow.createCell(3).setCellValue(new Date().toString());
-
-                        // Remova a linha da planilha de origem
-                        sourceSheet.removeRow(currentRow);
-
-                        // Salve as alterações no arquivo Excel
-                        FileOutputStream fos = new FileOutputStream(new File("dados.xlsx"));
-                        workbook.write(fos);
-                        fos.close();
-                        workbook.close();
-                        fis.close();
-                        System.out.println("OP " + op + " movida de " + sourceSheetName + " para " + targetSheetName);
-                        return;
-                    }
-                }
-            }
-
-            // Se não encontrou uma linha com a OP fornecida na planilha de origem
-            System.out.println("Nenhuma entrada encontrada para a OP " + op + " na planilha " + sourceSheetName);
-
-            workbook.close();
-            fis.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static void registerEntrada(String data, String ref, String op, String qntd, String frent, String tras, String silbor, String kamb, String sts) {
+    	
+    	File file = new File("dados.xlsx");
+    	boolean fileExists = file.exists();
+    	
+    	try {
+    		Workbook workbook;
+    		Sheet sheet;
+    		
+    		if (fileExists) {
+    			FileInputStream fis = new FileInputStream(file);
+    			workbook = new XSSFWorkbook(fis);
+    			sheet = workbook.getSheet("RegistroEntrada");
+    		} else {
+    			workbook = new XSSFWorkbook();
+    			sheet = workbook.createSheet("RegistroEntrada");
+    			Row headerRow = sheet.createRow(0);
+				headerRow.createCell(0).setCellValue("DATA");
+				headerRow.createCell(1).setCellValue("REFERENCIA");
+				headerRow.createCell(2).setCellValue("OP");
+				headerRow.createCell(3).setCellValue("QUANTIDADE");
+				headerRow.createCell(4).setCellValue("FRENT");
+				headerRow.createCell(5).setCellValue("TRAS");
+				headerRow.createCell(6).setCellValue("SILBOR");
+				headerRow.createCell(7).setCellValue("KAMBA");
+				headerRow.createCell(8).setCellValue("STATUS");
+    		}
+    		
+    		int lastRowNum = sheet.getLastRowNum();
+    		Row dataRow = sheet.createRow(lastRowNum + 1);
+    		dataRow.createCell(0).setCellValue(data);
+    		dataRow.createCell(1).setCellValue(ref);
+    		dataRow.createCell(2).setCellValue(op);
+    		dataRow.createCell(3).setCellValue(qntd);
+    		dataRow.createCell(4).setCellValue(frent);
+    		dataRow.createCell(5).setCellValue(tras);
+    		dataRow.createCell(6).setCellValue(silbor);
+    		dataRow.createCell(7).setCellValue(kamb);
+    		dataRow.createCell(8).setCellValue(sts);
+    		
+    		try (FileOutputStream fileOut = new FileOutputStream("dados.xlsx")) {
+    			workbook.write(fileOut);
+    			System.out.println("Dados inseridos no registro do dados.xlsx");
+    		}
+    	} catch (IOException e) {
+    		System.out.println("ERROR.: " + e.getMessage());
+    	}
     }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Digite a OP desejada: ");
-        String opDesejada = scanner.nextLine();
-        
-        // Suponha que você queira mover a OP para a planilha "Alocação"
-        migrateOp(opDesejada, "Entrada", "Alocação");
+    
+    public static void registerAloc(String ofi, String dtinicio, String ref, String op, String qntd, String dtfinal, String sts) {
+    	
+    	File file = new File("dados.xlsx");
+    	boolean fileExists = file.exists();
+    	
+    	try {
+    		Workbook workbook;
+    		Sheet sheet;
+    		
+    		if (fileExists) {
+    			FileInputStream fis = new FileInputStream(file);
+    			workbook = new XSSFWorkbook(fis);
+    			sheet = workbook.getSheet("RegistroAloc");
+    		} else {
+    			workbook = new XSSFWorkbook();
+    			sheet = workbook.createSheet("RegistroAloc");
+    			Row headerRow = sheet.createRow(0);
+    			headerRow.createCell(0).setCellValue("OFICINA");
+				headerRow.createCell(1).setCellValue("DT INICIO");
+				headerRow.createCell(2).setCellValue("REFERENCIA");
+				headerRow.createCell(3).setCellValue("OP");
+				headerRow.createCell(4).setCellValue("QUANTIDADE");
+				headerRow.createCell(5).setCellValue("DT FINAL");
+				headerRow.createCell(6).setCellValue("STATUS");
+    		}
+    		
+    		int lastRowNum = sheet.getLastRowNum();
+    		Row dataRow = sheet.createRow(lastRowNum + 1);
+    		dataRow.createCell(0).setCellValue(ofi);
+    		dataRow.createCell(1).setCellValue(dtinicio);
+    		dataRow.createCell(2).setCellValue(ref);
+    		dataRow.createCell(3).setCellValue(op);
+    		dataRow.createCell(4).setCellValue(qntd);
+    		dataRow.createCell(5).setCellValue(dtfinal);
+    		dataRow.createCell(6).setCellValue(sts);
+    		
+    		try (FileOutputStream fileOut = new FileOutputStream("dados.xlsx")) {
+    			workbook.write(fileOut);
+    			System.out.println("Dados inseridos no registro do dados.xlsx");
+    		}
+    	} catch (IOException e) {
+    		System.out.println("ERROR.: " + e.getMessage());
+    	}
     }
-}
+    
+    public static void registerEntrega(String data, String fac, String ref, String op, String qop, String qprod, String sts) {
+    	File file = new File("dados.xlsx");
+		boolean fileExists = file.exists();
+
+		try {
+			Workbook workbook;
+			Sheet sheet;
+
+			if (fileExists) {
+				FileInputStream fis = new FileInputStream(file);
+				workbook = new XSSFWorkbook(fis);
+				sheet = workbook.getSheet("RegistroEntrega");
+			} else {
+				workbook = new XSSFWorkbook();
+				sheet = workbook.createSheet("RegistroEntrega");
+				Row headerRow = sheet.createRow(0);
+				headerRow.createCell(0).setCellValue("DATA");
+				headerRow.createCell(1).setCellValue("FACCAO");
+				headerRow.createCell(2).setCellValue("REFERENCIA");
+				headerRow.createCell(3).setCellValue("OP");
+				headerRow.createCell(4).setCellValue("Q. OP.");
+				headerRow.createCell(5).setCellValue("Q. PROD.");
+				headerRow.createCell(6).setCellValue("STATUS");
+			}
+
+			int lastRowNum = sheet.getLastRowNum();
+			Row dataRow = sheet.createRow(lastRowNum + 1);
+			dataRow.createCell(0).setCellValue(data);
+			dataRow.createCell(1).setCellValue(fac);
+			dataRow.createCell(2).setCellValue(ref);
+			dataRow.createCell(3).setCellValue(op);
+			dataRow.createCell(4).setCellValue(qop);
+			dataRow.createCell(5).setCellValue(qprod);
+			dataRow.createCell(6).setCellValue(sts);
+
+			try (FileOutputStream fileOut = new FileOutputStream("dados.xlsx")) {
+				workbook.write(fileOut);
+				System.out.println("Dados exportados para o registroentrada");
+			}
+		} catch (IOException e) {
+			System.out.println("Erro ao fechar o workbook: " + e.getMessage());
+		}
+    }
+    }
