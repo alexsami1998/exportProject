b00b83c3c0c0a289d1bc3fe35c87c3533bbfcbe3
alexsami1998 <alexandre-enter86@live.com>
2023-09-16 17:28:03 -0300

    Ajustes nas dimensões para dimensões fixas.

--- exportProject/src/main/java/exportProjectInterface/Principal.java
+++ exportProject/src/main/java/exportProjectInterface/Principal.java
@@ -4,8 +4,6 @@ import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.FileInputStream;
-import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -15,35 +13,28 @@ import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
+import java.awt.Canvas;
 
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+public class Principal extends JFrame {
+    private JPanel contentPane;
 
-public class Gui extends JFrame {
-
-	private JPanel contentPane;
-	private final JButton btnNewButton = new JButton("Exibir OPS - Entrada");
-
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Gui frame = new Gui();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    Principal frame = new Principal();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
 
-	public Gui() {
+	public Principal() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 969, 658);
 
 		JMenuBar menuBar = new JMenuBar();
 		menuBar.setBackground(Color.LIGHT_GRAY);
@@ -59,53 +50,63 @@ public class Gui extends JFrame {
 		contentPane.add(list);
 
 		JButton btnNewButton_2 = new JButton("Adicionar dados no arquivo de ENTRADA");
-		btnNewButton_2.setBounds(74, 50, 301, 29);
+		btnNewButton_2.setBounds(310, 129, 301, 59);
 		btnNewButton_2.setHorizontalAlignment(SwingConstants.LEFT);
 		contentPane.add(btnNewButton_2);
 
 		btnNewButton_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				Gui2 gui2 = new Gui2();
-		        
-		        gui2.setVisible(true);
-			}
-		});
-		
-		JButton btnNewButton = new JButton("Exibir OPS - Entrada - Manut.");
-		btnNewButton.setBounds(74, 171, 301, 29);
-		contentPane.add(btnNewButton);
-		
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				EntradaListar entradaListar = new EntradaListar();
-		        
-		        entradaListar.setVisible(true);
-			}
-		});
+				SalvarEntrada salvarEntrada = new SalvarEntrada();
 
+		        salvarEntrada.setVisible(true);
+		        Principal.this.setVisible(false);
+			}
+		});
 
 
 		JMenuItem mntmNewMenuItem = new JMenuItem("Gerenciador de OPS");
-		mntmNewMenuItem.setBounds(130, 5, 183, 19);
+		mntmNewMenuItem.setBounds(371, 79, 183, 19);
 		contentPane.add(mntmNewMenuItem);
 
 		JButton btnNewButton_1 = new JButton("Exibir OPS - Alocação");
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				EntradaAloc entradaAloc = new EntradaAloc();
+				
+				entradaAloc.setVisible(true);
+				Principal.this.setVisible(false);
 			}
 		});
-		btnNewButton_1.setBounds(74, 130, 301, 29);
+		btnNewButton_1.setBounds(310, 286, 301, 50);
 		contentPane.add(btnNewButton_1);
-		btnNewButton.setBounds(74, 89, 301, 29);
-		contentPane.add(btnNewButton);
-
-		JButton btnNewButton_3 = new JButton("Deletar dados");
-		btnNewButton_3.setBounds(74, 215, 301, 29);
-		contentPane.add(btnNewButton_3);
 		
 		JButton btnNewButton_4 = new JButton("Exibir OPS - Entrega");
-		btnNewButton_4.setBounds(74, 171, 301, 29);
+		btnNewButton_4.setBounds(310, 367, 301, 52);
 		contentPane.add(btnNewButton_4);
-	}
-
+		
+		JButton btnNewButton_1_1 = new JButton("Exibir OPS - Entrada");
+		btnNewButton_1_1.setBounds(310, 211, 301, 50);
+		contentPane.add(btnNewButton_1_1);
+		
+		btnNewButton_1_1.addActionListener(new ActionListener () {
+			public void actionPerformed(ActionEvent e) {
+				EntradaListar entradaListar = new EntradaListar();
+				
+				entradaListar.setVisible(true);
+				Principal.this.setVisible(false);
+			}
+		});
+		
+		JButton btnNewButton_4_1 = new JButton("Sair");
+		btnNewButton_4_1.setForeground(Color.RED);
+		btnNewButton_4_1.setBounds(310, 550, 301, 52);
+		contentPane.add(btnNewButton_4_1);
+		
+		btnNewButton_4_1.addActionListener(new ActionListener () {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		
+			}
 }
