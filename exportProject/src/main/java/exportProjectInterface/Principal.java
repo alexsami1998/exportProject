9ff8a5e0669fdb0c255312c8c3917bb1f2965667
alexsami1998 <alexandre-enter86@live.com>
2023-09-19 16:39:25 -0300

    CLEAN CODE  - PRINCIPAL #26

--- exportProject/src/main/java/exportProjectInterface/Principal.java
+++ exportProject/src/main/java/exportProjectInterface/Principal.java
@@ -7,32 +7,30 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
-import java.awt.Canvas;
-import javax.swing.JSeparator;
-import javax.swing.JLabel;
 
 public class Principal extends JFrame {
-    private JPanel contentPane;
+	private JPanel contentPane;
 
-    public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    Principal frame = new Principal();
-                    frame.setVisible(true);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
-
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Principal frame = new Principal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
 	public Principal() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -51,104 +49,128 @@ public class Principal extends JFrame {
 		list.setBounds(74, 24, 0, 0);
 		contentPane.add(list);
 
-		JButton btnNewButton_2 = new JButton("Adicionar dados no arquivo de ENTRADA");
-		btnNewButton_2.setForeground(Color.BLUE);
-		btnNewButton_2.setBounds(310, 55, 301, 74);
-		btnNewButton_2.setHorizontalAlignment(SwingConstants.LEFT);
-		contentPane.add(btnNewButton_2);
+		JButton btnAdicionarDados = new JButton("Adicionar dados no arquivo de ENTRADA");
+		btnAdicionarDados.setForeground(Color.BLUE);
+		btnAdicionarDados.setBounds(310, 55, 301, 74);
+		btnAdicionarDados.setHorizontalAlignment(SwingConstants.LEFT);
+		contentPane.add(btnAdicionarDados);
 
-		btnNewButton_2.addActionListener(new ActionListener() {
+		btnAdicionarDados.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				Principal.this.setVisible(false);
+				
 				SalvarEntrada salvarEntrada = new SalvarEntrada();
-
-		        salvarEntrada.setVisible(true);
-		        Principal.this.setVisible(false);
+				salvarEntrada.setVisible(true);
 			}
 		});
-
 
 		JMenuItem mntmNewMenuItem = new JMenuItem("Gerenciador de OPS");
 		mntmNewMenuItem.setBounds(370, 24, 183, 19);
 		contentPane.add(mntmNewMenuItem);
 
-		JButton btnNewButton_1 = new JButton("Exibir OPS - Alocação");
-		btnNewButton_1.addActionListener(new ActionListener() {
+		JButton btnAlocacao = new JButton("Exibir OPS - Alocação");
+		btnAlocacao.setBounds(310, 227, 301, 50);
+		contentPane.add(btnAlocacao);
+		btnAlocacao.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				EntradaAloc entradaAloc = new EntradaAloc();
-				
-				entradaAloc.setVisible(true);
 				Principal.this.setVisible(false);
+				
+				EntradaAloc entradaAloc = new EntradaAloc();
+				entradaAloc.setVisible(true);
 			}
 		});
-		btnNewButton_1.setBounds(310, 227, 301, 50);
-		contentPane.add(btnNewButton_1);
-		
+
 		JButton btnEntrega = new JButton("Exibir OPS - Entrega");
 		btnEntrega.setBounds(310, 302, 301, 52);
 		contentPane.add(btnEntrega);
-		
-		btnEntrega.addActionListener(new ActionListener () {
+
+		btnEntrega.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				Principal.this.setVisible(false);
+				
 				EntregaListar entregaListar = new EntregaListar();
 				entregaListar.setVisible(true);
-				
-				Principal.this.setVisible(false);
 			}
 		});
-		
-		
-		
-		JButton btnNewButton_1_1 = new JButton("Exibir OPS - Entrada");
-		btnNewButton_1_1.setBounds(310, 155, 301, 50);
-		contentPane.add(btnNewButton_1_1);
-		
-		btnNewButton_1_1.addActionListener(new ActionListener () {
+
+		JButton btnEntradaListar = new JButton("Exibir OPS - Entrada");
+		btnEntradaListar.setBounds(310, 155, 301, 50);
+		contentPane.add(btnEntradaListar);
+
+		btnEntradaListar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				EntradaListar entradaListar = new EntradaListar();
-				
-				entradaListar.setVisible(true);
 				Principal.this.setVisible(false);
+				
+				EntradaListar entradaListar = new EntradaListar();
+				entradaListar.setVisible(true);
 			}
 		});
-		
-		JButton btnNewButton_4_1 = new JButton("Sair");
-		btnNewButton_4_1.setForeground(Color.RED);
-		btnNewButton_4_1.setBounds(310, 550, 301, 52);
-		contentPane.add(btnNewButton_4_1);
-		
-		JButton btnEntrega_1 = new JButton("ENTRADA");
-		btnEntrega_1.setBounds(310, 403, 301, 29);
-		contentPane.add(btnEntrega_1);
-		
+
+		JButton btnEntradaReg = new JButton("ENTRADA");
+		btnEntradaReg.setBounds(386, 403, 142, 29);
+		contentPane.add(btnEntradaReg);
+
+		btnEntradaReg.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Principal.this.setVisible(false);
+				
+				RegistroEntrada registroEntrada = new RegistroEntrada();
+				registroEntrada.setVisible(true);
+			}
+		});
+
 		JSeparator separator_1_2 = new JSeparator();
 		separator_1_2.setBounds(331, 359, 255, 12);
 		contentPane.add(separator_1_2);
-		
+
 		JSeparator separator_1_2_1 = new JSeparator();
 		separator_1_2_1.setBounds(331, 366, 255, 12);
 		contentPane.add(separator_1_2_1);
-		
-		JButton btnEntrega_1_1 = new JButton("ALOCAÇÃO");
-		btnEntrega_1_1.setBounds(310, 444, 301, 29);
-		contentPane.add(btnEntrega_1_1);
-		
-		JButton btnEntrega_1_2 = new JButton("ENTREGA");
-		btnEntrega_1_2.setBounds(310, 485, 301, 29);
-		contentPane.add(btnEntrega_1_2);
-		
+
+		JButton btnAlocacaoReg = new JButton("ALOCAÇÃO");
+		btnAlocacaoReg.setBounds(386, 444, 142, 29);
+		contentPane.add(btnAlocacaoReg);
+
+		btnAlocacaoReg.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Principal.this.setVisible(false);
+				
+				RegistroAlocacao registroAlocacao = new RegistroAlocacao();
+				registroAlocacao.setVisible(true);
+			}
+		});
+
+		JButton btnEntregaReg = new JButton("ENTREGA");
+		btnEntregaReg.setBounds(386, 485, 142, 29);
+		contentPane.add(btnEntregaReg);
+
+		btnEntregaReg.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Principal.this.setVisible(false);
+				
+				RegistroEntrega registroEntrega = new RegistroEntrega();
+				registroEntrega.setVisible(true);
+			}
+		});
+
 		JLabel lblNewLabel = new JLabel("HISTÓRICO");
 		lblNewLabel.setBounds(423, 376, 88, 16);
 		contentPane.add(lblNewLabel);
-		
+
 		JLabel lblVersion = new JLabel("version - 1.0.1");
 		lblVersion.setBounds(867, 586, 96, 16);
 		contentPane.add(lblVersion);
 		
-		btnNewButton_4_1.addActionListener(new ActionListener () {
+		JButton btnSair = new JButton("Sair");
+		btnSair.setForeground(Color.RED);
+		btnSair.setBounds(310, 550, 301, 52);
+		contentPane.add(btnSair);
+
+		btnSair.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.exit(0);
 			}
 		});
-		
-			}
+
+	}
 }
