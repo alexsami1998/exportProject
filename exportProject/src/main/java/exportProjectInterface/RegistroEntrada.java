ac44018c2b2a378d670622b466e2a43f04f0d32d
alexsami1998 <alexandre-enter86@live.com>
2023-09-19 17:01:32 -0300

    CLEAN CODE - REGISTROENTRADA #32

--- exportProject/src/main/java/exportProjectInterface/RegistroEntrada.java
+++ exportProject/src/main/java/exportProjectInterface/RegistroEntrada.java
@@ -25,6 +25,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class RegistroEntrada extends JFrame {
 
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTable table;
 
@@ -99,6 +100,7 @@ public class RegistroEntrada extends JFrame {
 		btnVoltar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				RegistroEntrada.this.setVisible(false);
+				
 				Principal principal = new Principal();
 				principal.setVisible(true);
 			}
