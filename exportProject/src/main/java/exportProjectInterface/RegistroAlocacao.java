595ca5ddfffd2265d0e869c4e1f36af49c5f8da1
alexsami1998 <alexandre-enter86@live.com>
2023-09-19 16:59:52 -0300

    CLEAN CODE - REGISTROALOCACAO #31

--- exportProject/src/main/java/exportProjectInterface/RegistroAlocacao.java
+++ exportProject/src/main/java/exportProjectInterface/RegistroAlocacao.java
@@ -25,6 +25,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class RegistroAlocacao extends JFrame {
 
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTable table;
 
@@ -99,6 +100,7 @@ public class RegistroAlocacao extends JFrame {
 		btnVoltar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				RegistroAlocacao.this.setVisible(false);
+				
 				Principal principal = new Principal();
 				principal.setVisible(true);
 			}
