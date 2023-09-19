75cf45ba2088c328f8b265df7e669018e3115268
alexsami1998 <alexandre-enter86@live.com>
2023-09-19 00:48:10 -0300

    Ajustes graficos

--- exportProject/src/main/java/exportProjectInterface/Principal.java
+++ exportProject/src/main/java/exportProjectInterface/Principal.java
@@ -14,6 +14,8 @@ import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 import java.awt.Canvas;
+import javax.swing.JSeparator;
+import javax.swing.JLabel;
 
 public class Principal extends JFrame {
     private JPanel contentPane;
@@ -50,7 +52,8 @@ public class Principal extends JFrame {
 		contentPane.add(list);
 
 		JButton btnNewButton_2 = new JButton("Adicionar dados no arquivo de ENTRADA");
-		btnNewButton_2.setBounds(310, 129, 301, 59);
+		btnNewButton_2.setForeground(Color.BLUE);
+		btnNewButton_2.setBounds(310, 55, 301, 74);
 		btnNewButton_2.setHorizontalAlignment(SwingConstants.LEFT);
 		contentPane.add(btnNewButton_2);
 
@@ -65,7 +68,7 @@ public class Principal extends JFrame {
 
 
 		JMenuItem mntmNewMenuItem = new JMenuItem("Gerenciador de OPS");
-		mntmNewMenuItem.setBounds(371, 79, 183, 19);
+		mntmNewMenuItem.setBounds(370, 24, 183, 19);
 		contentPane.add(mntmNewMenuItem);
 
 		JButton btnNewButton_1 = new JButton("Exibir OPS - Alocação");
@@ -77,11 +80,11 @@ public class Principal extends JFrame {
 				Principal.this.setVisible(false);
 			}
 		});
-		btnNewButton_1.setBounds(310, 286, 301, 50);
+		btnNewButton_1.setBounds(310, 227, 301, 50);
 		contentPane.add(btnNewButton_1);
 		
 		JButton btnEntrega = new JButton("Exibir OPS - Entrega");
-		btnEntrega.setBounds(310, 367, 301, 52);
+		btnEntrega.setBounds(310, 302, 301, 52);
 		contentPane.add(btnEntrega);
 		
 		btnEntrega.addActionListener(new ActionListener () {
@@ -96,7 +99,7 @@ public class Principal extends JFrame {
 		
 		
 		JButton btnNewButton_1_1 = new JButton("Exibir OPS - Entrada");
-		btnNewButton_1_1.setBounds(310, 211, 301, 50);
+		btnNewButton_1_1.setBounds(310, 155, 301, 50);
 		contentPane.add(btnNewButton_1_1);
 		
 		btnNewButton_1_1.addActionListener(new ActionListener () {
@@ -112,6 +115,34 @@ public class Principal extends JFrame {
 		btnNewButton_4_1.setForeground(Color.RED);
 		btnNewButton_4_1.setBounds(310, 550, 301, 52);
 		contentPane.add(btnNewButton_4_1);
+		
+		JButton btnEntrega_1 = new JButton("ENTRADA");
+		btnEntrega_1.setBounds(310, 403, 301, 29);
+		contentPane.add(btnEntrega_1);
+		
+		JSeparator separator_1_2 = new JSeparator();
+		separator_1_2.setBounds(331, 359, 255, 12);
+		contentPane.add(separator_1_2);
+		
+		JSeparator separator_1_2_1 = new JSeparator();
+		separator_1_2_1.setBounds(331, 366, 255, 12);
+		contentPane.add(separator_1_2_1);
+		
+		JButton btnEntrega_1_1 = new JButton("ALOCAÇÃO");
+		btnEntrega_1_1.setBounds(310, 444, 301, 29);
+		contentPane.add(btnEntrega_1_1);
+		
+		JButton btnEntrega_1_2 = new JButton("ENTREGA");
+		btnEntrega_1_2.setBounds(310, 485, 301, 29);
+		contentPane.add(btnEntrega_1_2);
+		
+		JLabel lblNewLabel = new JLabel("HISTÓRICO");
+		lblNewLabel.setBounds(423, 376, 88, 16);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblVersion = new JLabel("version - 1.0.1");
+		lblVersion.setBounds(867, 586, 96, 16);
+		contentPane.add(lblVersion);
 		
 		btnNewButton_4_1.addActionListener(new ActionListener () {
 			public void actionPerformed(ActionEvent e) {
