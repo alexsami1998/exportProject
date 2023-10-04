02be865fccf4505cb4a6db0d4d4fe2c7c9612b64
alexsami1998 <alexandre-enter86@live.com>
2023-10-03 23:01:56 -0300

    Integrando classes de insercao de dados a apenas uma classe somente.

--- exportProject/src/main/java/exportProject/ManagerData.java
+++ exportProject/src/main/java/exportProject/ManagerData.java
@@ -69,4 +69,109 @@ public class ManagerData {
 			System.out.println("Erro ao fechar o workbook: " + e.getMessage());
 		}
 	}
+	
+	@SuppressWarnings("resource")
+	public static void addEntryAloc(String ref, String op, String qntd, String sts) {
+
+		String ofi = null;
+		String dtfinal = null;
+
+		File file = new File("dados.xlsx");
+		boolean fileExists = file.exists();
+
+		try {
+			Workbook workbook;
+			Sheet sheet;
+
+			if (fileExists) {
+				FileInputStream fis = new FileInputStream(file);
+				workbook = new XSSFWorkbook(fis);
+				sheet = workbook.getSheet("Alocacao");
+			} else {
+				workbook = new XSSFWorkbook();
+				sheet = workbook.createSheet("Alocacao");
+				Row headerRow = sheet.createRow(0);
+				headerRow.createCell(0).setCellValue("OFICINA");
+				headerRow.createCell(1).setCellValue("DT INICIO");
+				headerRow.createCell(2).setCellValue("REFERENCIA");
+				headerRow.createCell(3).setCellValue("OP");
+				headerRow.createCell(4).setCellValue("QUANTIDADE");
+				headerRow.createCell(5).setCellValue("DT FINAL");
+				headerRow.createCell(6).setCellValue("STATUS");
+			}
+
+			LocalDateTime dataHoraAtual = LocalDateTime.now();
+	        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+	        String dtInicio = dataHoraAtual.format(formatter);
+			int lastRowNum = sheet.getLastRowNum();
+			Row dataRow = sheet.createRow(lastRowNum + 1);
+			dataRow.createCell(0).setCellValue(ofi);
+			dataRow.createCell(1).setCellValue(dtInicio);
+			dataRow.createCell(2).setCellValue(ref);
+			dataRow.createCell(3).setCellValue(op);
+			dataRow.createCell(4).setCellValue(qntd);
+			dataRow.createCell(5).setCellValue(dtfinal);
+			dataRow.createCell(6).setCellValue(sts);
+
+			try (FileOutputStream fileOut = new FileOutputStream("dados.xlsx")) {
+				workbook.write(fileOut);
+				System.out.println("Dados exportados para o arquivo dados.xlsx");
+			}
+		} catch (IOException e) {
+			System.out.println("Erro ao fechar o workbook: " + e.getMessage());
+		}
+	}
+	
+	@SuppressWarnings("resource")
+	public static void addEntryEntrega(String ref, String op, String sts) {
+		String fac = null;
+		String qop = null;
+		String qprod = null;
+		
+
+		File file = new File("dados.xlsx");
+		boolean fileExists = file.exists();
+
+		try {
+			Workbook workbook;
+			Sheet sheet;
+
+			if (fileExists) {
+				FileInputStream fis = new FileInputStream(file);
+				workbook = new XSSFWorkbook(fis);
+				sheet = workbook.getSheet("Entrega");
+			} else {
+				workbook = new XSSFWorkbook();
+				sheet = workbook.createSheet("Entrega");
+				Row headerRow = sheet.createRow(0);
+				headerRow.createCell(0).setCellValue("DATA");
+				headerRow.createCell(1).setCellValue("FACCAO");
+				headerRow.createCell(2).setCellValue("REFERENCIA");
+				headerRow.createCell(3).setCellValue("OP");
+				headerRow.createCell(4).setCellValue("Q. OP.");
+				headerRow.createCell(5).setCellValue("Q. PROD.");
+				headerRow.createCell(6).setCellValue("STATUS");
+			}
+			LocalDateTime dataHoraAtual = LocalDateTime.now();
+	        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+	        String dataEntrega = dataHoraAtual.format(formatter);
+
+			int lastRowNum = sheet.getLastRowNum();
+			Row dataRow = sheet.createRow(lastRowNum + 1);
+			dataRow.createCell(0).setCellValue(dataEntrega);
+			dataRow.createCell(1).setCellValue(fac);
+			dataRow.createCell(2).setCellValue(ref);
+			dataRow.createCell(3).setCellValue(op);
+			dataRow.createCell(4).setCellValue(qop);
+			dataRow.createCell(5).setCellValue(qprod);
+			dataRow.createCell(6).setCellValue(sts);
+
+			try (FileOutputStream fileOut = new FileOutputStream("dados.xlsx")) {
+				workbook.write(fileOut);
+				System.out.println("Dados exportados para o arquivo dados.xlsx");
+			}
+		} catch (IOException e) {
+			System.out.println("Erro ao fechar o workbook: " + e.getMessage());
+		}
+	}
 }
