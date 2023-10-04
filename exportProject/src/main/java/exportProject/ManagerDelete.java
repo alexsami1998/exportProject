41500e2e7343fdabe8e926720cdaea8d3ec727ea
alexsami1998 <alexandre-enter86@live.com>
2023-10-03 23:00:43 -0300

    Integrando classe de delecao para uma apenas. Clean code.

--- exportProject/src/main/java/exportProject/ManagerDelete.java
+++ exportProject/src/main/java/exportProject/ManagerDelete.java
@@ -32,7 +32,6 @@ public class ManagerDelete {
                     String opValue = opCell.getStringCellValue();
 
                     if (opValue.equals(op)) {
-                        // Se a OP encontrada corresponder à OP fornecida, exclua a linha
                         sheet.removeRow(currentRow);
                         System.out.println("Entrada com OP " + op + " excluída com sucesso.");
                         FileOutputStream fos = new FileOutputStream(new File("dados.xlsx"));
@@ -40,12 +39,11 @@ public class ManagerDelete {
                         fos.close();
                         workbook.close();
                         fis.close();
-                        return; // Encontrou a linha, não é necessário continuar a busca
+                        return;
                     }
                 }
             }
 
-            // Se não encontrou uma linha com a OP fornecida
             System.out.println("Nenhuma entrada encontrada para a OP: " + op);
 
             workbook.close();
@@ -54,6 +52,80 @@ public class ManagerDelete {
             e.printStackTrace();
         }
     }
+    
+    public static void deleteEntryByAloc(String op) {
+		try {
+			FileInputStream fis = new FileInputStream(new File("dados.xlsx"));
+			Workbook workbook = new XSSFWorkbook(fis);
+			Sheet sheet = workbook.getSheet("Alocacao");
+
+			Iterator<Row> iterator = sheet.iterator();
+
+			while (iterator.hasNext()) {
+				Row currentRow = iterator.next();
+				Cell opCell = currentRow.getCell(3); // Coluna OP
+
+				if (opCell != null && opCell.getCellType() == CellType.STRING) {
+					String opValue = opCell.getStringCellValue();
+
+					if (opValue.equals(op)) {
+						sheet.removeRow(currentRow);
+						System.out.println("Entrada com OP " + op + " excluída com sucesso.");
+						FileOutputStream fos = new FileOutputStream(new File("dados.xlsx"));
+						workbook.write(fos);
+						fos.close();
+						workbook.close();
+						fis.close();
+						return; 
+					}
+				}
+			}
+
+			System.out.println("Nenhuma entrada encontrada para a OP: " + op);
+
+			workbook.close();
+			fis.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void deleteEntryByEntrega(String op) {
+		try {
+			FileInputStream fis = new FileInputStream(new File("dados.xlsx"));
+			Workbook workbook = new XSSFWorkbook(fis);
+			Sheet sheet = workbook.getSheet("Entrega");
+
+			Iterator<Row> iterator = sheet.iterator();
+
+			while (iterator.hasNext()) {
+				Row currentRow = iterator.next();
+				Cell opCell = currentRow.getCell(3);
+
+				if (opCell != null && opCell.getCellType() == CellType.STRING) {
+					String opValue = opCell.getStringCellValue();
+
+					if (opValue.equals(op)) {
+						sheet.removeRow(currentRow);
+						System.out.println("Entrada com OP " + op + " excluída com sucesso.");
+						FileOutputStream fos = new FileOutputStream(new File("dados.xlsx"));
+						workbook.write(fos);
+						fos.close();
+						workbook.close();
+						fis.close();
+						return;
+					}
+				}
+			}
+
+			System.out.println("Nenhuma entrada encontrada para a OP: " + op);
+
+			workbook.close();
+			fis.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
     public static void main(String[] args) {
         @SuppressWarnings("resource")
@@ -61,6 +133,8 @@ public class ManagerDelete {
         System.out.print("Digite a OP que deseja excluir: ");
         String opToDelete = scanner.nextLine();
 
+        deleteEntryByAloc(opToDelete);
+		deleteEntryByEntrega(opToDelete);
         deleteEntryByOp(opToDelete);
     }
 }
